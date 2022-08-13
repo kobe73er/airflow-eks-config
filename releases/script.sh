041ae0f101ff85435277487a7e0a8b2ec80870eb
andrew <andrew@appopay.com>
2022-08-13 10:05:13 +0800

    test

---
+++ releases/script.sh
@@ -0,0 +1,7 @@
+#You may create Secret/airflow-http-git-secret using this command,
+# replacing MY_GIT_USERNAME and MY_GIT_TOKEN with your HTTP credentials:
+kubectl create secret generic \
+  airflow-http-git-secret \
+  --from-literal=username='kobe73er' \
+  --from-literal=password='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' \
+  --namespace dev
